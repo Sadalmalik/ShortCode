7a85ebd5cfcdeb7a07d51a423cc781ed1efba2cb
Gleb Kuzmin <kuzmin@owlcat.games>
2026-01-12 21:13:28 +0100

    Second Iteration on algorythmic animations

--- Unity/GearBoxAnimations/Assets/Projects/YouTube_EP0_CSV/Code/SceneDriver.cs
+++ Unity/GearBoxAnimations/Assets/Projects/YouTube_EP0_CSV/Code/SceneDriver.cs
@@ -5,40 +5,104 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum Mode
+{
+    Sequential = 0,
+    Parallel = 1,
+}
+
+public enum HandleMode
+{
+    AnchorPos,
+    SizeDelta
+}
+
+[Serializable]
+public class Handle
+{
+    public RectTransform handle;
+    public HandleMode mode;
+    public Vector2 endValue;
+    public float duration;
+    
+    public Tween DoTween()
+    {
+        switch (mode)
+        {
+            case HandleMode.AnchorPos:
+                return handle.DOAnchorPos(endValue, duration).SetEase(Ease.InOutQuad);
+            case HandleMode.SizeDelta:
+                return handle.DOSizeDelta(endValue, duration).SetEase(Ease.InOutQuad);
+        }
+
+        return null;
+    }
+}
+
+[Serializable]
+public class HandlesGroup
+{
+    public Mode mode=Mode.Parallel;
+    public List<Handle> Handles;
+    public float endPause;
+    
+    public float DoTween(Sequence sequence, float time)
+    {
+        switch (mode)
+        {
+            case Mode.Sequential:
+                foreach (var handle in Handles)
+                {
+                    sequence.Insert(time, handle.DoTween());
+                    time += handle.duration;
+                }
+                break;
+            case Mode.Parallel:
+                var max = 0f;
+                foreach (var handle in Handles)
+                {
+                    sequence.Insert(time, handle.DoTween());
+                    max = Mathf.Max(max, handle.duration);
+                }
+                time += max;
+                break;
+        }
+
+        return time + endPause;
+    }
+}
+
 public class SceneDriver : MonoBehaviour
 {
     public List<RectTransform> Layouts;
-    
-    public List<RectTransform> targets;
-    public List<TMP_Text> pops;
+
+    [Space]
     public float inPause = 2f;
+    public List<HandlesGroup> Groups;
+
+    [Space]
+    public List<RectTransform> targets;
     public float duration = 1f;
     public float delay = 0.1f;
-    public float popDelay = 0.1f;
-    public Vector2 targetSizeDelta = new Vector2(250, 0);
+    public Vector2 targetSizeDelta = new Vector2(350, 0);
     
     private void Start()
     {
         var index = 0;
         var sequence = DOTween.Sequence();
+        var startTime = inPause;
         
+        foreach (var group in Groups)
+        {
+            startTime = group.DoTween(sequence, startTime);
+        }
+        
+        // Do rest splits
         foreach (var target in targets)
         {
             sequence.Insert(
-                inPause + index * delay,
-                target.DOSizeDelta(targetSizeDelta, duration));
-            index++;
-        }
-
-        index = 0;
-        foreach (var pop in pops)
-        {
-            sequence.Insert(
-                inPause + index * delay + popDelay,
-                pop.DOFade(1, duration * 0.01f));
-            sequence.Insert(
-                inPause + index * delay + popDelay + duration * 0.25f,
-                pop.DOFade(0, duration * 0.15f));
+                startTime + index * delay,
+                target.DOSizeDelta(targetSizeDelta, duration).SetEase(Ease.InOutQuad));
             index++;
         }
     }
