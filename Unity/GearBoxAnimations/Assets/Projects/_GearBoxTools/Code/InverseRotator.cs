1eac0bcdb68461125522e5bfc2da63232cc9c015
Gleb Kuzmin <kuzmin@owlcat.games>
2026-01-16 21:58:13 +0100

    Animations in progress

---
+++ Unity/GearBoxAnimations/Assets/Projects/_GearBoxTools/Code/InverseRotator.cs
@@ -0,0 +1,16 @@
+﻿using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace GearBoxTools
+{
+    [ExecuteInEditMode]
+    public class InverseRotator : SerializedMonoBehaviour
+    {
+        private void Update()
+        {
+            if (transform.parent == null)
+                return;
+            transform.localRotation = Quaternion.Inverse(transform.parent.localRotation);
+        }
+    }
+}
